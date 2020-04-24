46862639a1d87f36f4f06c83a145a29c902c4125
Nicolas A. Batara <nick.batara@gmail.com>
2020-04-24 19:23:43 -0400

    Created project in Xcode as Augmented Reality App using Swift Language

---
+++ faceMask/faceMask/faceMask-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  faceMask-Bridging-Header.h
+//  faceMask
+//
+//  Created by Nicolas Batara on 4/24/20.
+//  Copyright © 2020 Nicolas Batara. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ faceMask/faceMaskTests/faceMaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  faceMaskTests.swift
+//  faceMaskTests
+//
+//  Created by Nicolas Batara on 4/24/20.
+//  Copyright © 2020 Nicolas Batara. All rights reserved.
+//
+
+import XCTest
+@testable import faceMask
+
+class faceMaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
